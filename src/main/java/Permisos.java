05f65789605f81bbe68580304d1de7997d44918d
Claudia <162599317+claudiajimdom@users.noreply.github.com>
2026-01-04 19:31:39 +0100

    Add permissions for multiple users on various tables

--- src/main/java/Permisos.java
+++ src/main/java/Permisos.java
@@ -1,1 +1,8 @@
-
+GRANT SELECT, INSERT, UPDATE, DELETE ON Sucursal TO cerveza1, cerveza2, cerveza3, cerveza4;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Productor TO cerveza1, cerveza2, cerveza3, cerveza4;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Vino TO cerveza1, cerveza2, cerveza3, cerveza4;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Empleado TO cerveza1, cerveza2, cerveza3, cerveza4;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Cliente TO cerveza1, cerveza2, cerveza3, cerveza4;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Suministra TO cerveza1, cerveza2, cerveza3, cerveza4;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Solicita TO cerveza1, cerveza2, cerveza3, cerveza4;
+GRANT SELECT, INSERT, UPDATE, DELETE ON Pide TO cerveza1, cerveza2, cerveza3, cerveza4;
